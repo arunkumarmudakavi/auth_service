0567266ff5533cb4880479911cf17a9d52be5c1f
arunkumarmudakavi <arunkumarmudakavi029@gmail.com>
2026-05-13 16:39:17 +0100

    feat(DTO): add registration DTO

---
+++ src/main/java/com/example/assignment/dto/RegisterReq.java
@@ -0,0 +1,32 @@
+package com.example.assignment.dto;
+
+public class RegisterReq {
+
+    private String email;
+    private String password;
+
+    public RegisterReq(){
+
+    }
+
+    public RegisterReq(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+}

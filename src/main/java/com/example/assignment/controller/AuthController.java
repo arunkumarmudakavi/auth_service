fc6c4c70034b77ee6af10047f8207edf7eb8550b
arunkumarmudakavi <arunkumarmudakavi029@gmail.com>
2026-05-13 16:40:36 +0100

    feat(controller): implement AuthController for user authentication

---
+++ src/main/java/com/example/assignment/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.example.assignment.controller;
+
+import com.example.assignment.dto.LoginReq;
+import com.example.assignment.dto.RegisterReq;
+import com.example.assignment.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterReq request){
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginReq request){
+        return authService.login(request);
+    }
+}

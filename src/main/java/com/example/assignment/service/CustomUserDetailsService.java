11173df9d80d99497fb71303710edc5d541c08d1
arunkumarmudakavi <arunkumarmudakavi029@gmail.com>
2026-05-14 10:19:48 +0100

    feat(service): implement CustomUserDetailsService for user authentication

---
+++ src/main/java/com/example/assignment/service/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.example.assignment.service;
+
+import com.example.assignment.entity.User;
+import com.example.assignment.repository.UserRepository;
+import com.example.assignment.utils.exception.ResourceNotFoundException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(
+            UserRepository userRepository
+    ) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("User Not Found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singleton(
+                        new SimpleGrantedAuthority("USER")
+                )
+        );
+    }
+}

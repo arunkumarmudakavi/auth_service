c5ce4d2903cdb0547c0f2c9901ce745a47058c4e
arunkumarmudakavi <arunkumarmudakavi029@gmail.com>
2026-05-14 10:16:18 +0100

    feat(config): update SecurityConfig to enforce stateless session management

--- src/main/java/com/example/assignment/config/SecurityConfig.java
+++ src/main/java/com/example/assignment/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,23 +23,27 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public AuthenticationManager authenticationManager(
-        AuthenticationConfiguration config
-    ) throws Exception{
+            AuthenticationConfiguration config
+    ) throws Exception {
         return config.getAuthenticationManager();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
                 .authorizeHttpRequests(auth -> auth
 
                         // public APIs
@@ -46,7 +51,7 @@ public class SecurityConfig {
                                 "/auth/**"
                         ).permitAll()
 
-//                       // protected APIs
+                        // protected APIs
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(
